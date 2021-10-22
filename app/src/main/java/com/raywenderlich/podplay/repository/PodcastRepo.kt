7588ebba6886a72cb90746e9dff9c6ae94e9a75c
jhenriquedsilva <josehs555@gmail.com>
2021-10-22 13:56:03 -0300

    PodcastRepo is responsible for retrieving the podcast details and returning it to the view model

---
+++ app/src/main/java/com/raywenderlich/podplay/repository/PodcastRepo.kt
@@ -0,0 +1,10 @@
+package com.raywenderlich.podplay.repository
+
+import com.raywenderlich.podplay.model.Podcast
+
+// Retrieves the podcast details and returns them to the view model
+class PodcastRepo {
+    fun getPodcast(feedUrl: String): Podcast? {
+        return Podcast(feedUrl, "No name", "No description", "No image")
+    }
+}
